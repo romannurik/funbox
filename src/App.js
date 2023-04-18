447fd6f4304abb3b36ae9a1461362e9138800e92
Roman Nurik <roman@nurik.net>
2023-04-17 21:14:33 -0400

    spin off kidlang

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Redirect, Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import { KidLangSandbox } from "./pages/kidlang/KidLangSandbox";
 import { MathGame } from "./pages/math/MathGame";
 import { MathPlay } from "./pages/math/MathPlay";
 import { RobotGame } from "./pages/robot/RobotGame";
@@ -12,21 +11,11 @@ export function App() {
         <Route path="/mathplay" component={MathPlay} />
         <Route path="/mathgame" component={MathGame} />
         <Route path="/robot" component={RobotGame} />
-        <Route path="/kidlang" component={KidLangSandbox} />
+        <Route path="/kidlang">
+          Moved to <a href="https://kidlang.web.app">separate app</a>
+        </Route>
         <Route path="/">
           <Redirect to="/robot" />
-          {/* <div>
-            Available in the funbox:
-            <p>
-              <Link to="/mathplay">Math blocks</Link>
-            </p>
-            <p>
-              <Link to="/mathgame">Math game</Link>
-            </p>
-            <p>
-              <Link to="/robot">Robot programming game</Link>
-            </p>
-          </div> */}
         </Route>
       </Switch>
     </Router>
